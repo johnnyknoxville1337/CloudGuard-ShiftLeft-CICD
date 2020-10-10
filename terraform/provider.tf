3e5422d82b04d8dc9b7ea258ffbc0c593e251670
johhnay <41755735+johnnyknoxville1337@users.noreply.github.com>
2020-10-10 16:24:19 +0200

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
-provider "aws" {
-  region = "eu-central-1"
-  access_key = "AKIA2H7U475L74XE3TLM"
-  secret_key = "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"
-}
+#provider "aws" {
+#  region = "eu-central-1"
+#  access_key = "AKIA2H7U475L74XE3TLM"
+#  secret_key = "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"
+#}
